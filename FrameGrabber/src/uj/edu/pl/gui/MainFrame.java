66a3c03439cee9b27e72b53c567cb0ad720f1d59
Kamil <kk.up.krk@gmail.com>
2013-11-22 13:45:40 +0100

    Adding new privarevaribles of GUI

--- FrameGrabber/src/uj/edu/pl/gui/MainFrame.java
+++ FrameGrabber/src/uj/edu/pl/gui/MainFrame.java
@@ -6,6 +6,9 @@ import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JSlider;
 
 public class MainFrame extends JFrame {
 	private JButton stop;
@@ -13,7 +16,15 @@ public class MainFrame extends JFrame {
 	private JButton convert;
 	private GridBagConstraints gBC;
 	private GridBagLayout gBL;
-
+	private JSlider slider1;
+	private JLabel sliderLabel;
+	private JLabel patchLabel;
+	private JLabel infoLabel;
+	
+	
+	private JRadioButton typeOfSource;
+	private JRadioButton typeOfPicture;
+	private JRadioButton typeOfMovie;
 	public MainFrame() {
 		super("Frame Grabber");
 		gBC = new GridBagConstraints();
