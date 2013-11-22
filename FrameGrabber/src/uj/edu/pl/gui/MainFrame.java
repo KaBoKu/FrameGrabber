b96ecfefdf22ee5c49bd7e5f08f6b1efce2c88d2
Kamil <kk.up.krk@gmail.com>
2013-11-22 14:09:18 +0100

    Make setButtons function

--- FrameGrabber/src/uj/edu/pl/gui/MainFrame.java
+++ FrameGrabber/src/uj/edu/pl/gui/MainFrame.java
@@ -3,7 +3,9 @@ package uj.edu.pl.gui;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.KeyEvent;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,7 +22,7 @@ public class MainFrame extends JFrame {
 	private JLabel sliderLabel;
 	private JLabel patchLabel;
 	private JLabel infoLabel;
-	
+	private ButtonGroup group;
 	
 	private JRadioButton typeOfSource;
 	private JRadioButton typeOfPicture;
@@ -29,10 +31,28 @@ public class MainFrame extends JFrame {
 		super("Frame Grabber");
 		gBC = new GridBagConstraints();
 		gBL = new GridBagLayout();
+		setLayout(gBL);
+		gBC.fill = GridBagConstraints.NONE;
 		Dimension dimension = new Dimension(810, 320);
 		this.setSize(dimension);
 		this.setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+	
+	private void SetButtons(){
+		stop = new JButton("Stop");
+		preview = new JButton("Perview");
+		convert = new JButton("Convert");
+		
+		//Tooltips set
+		stop.setToolTipText("Press to stop");
+		preview.setToolTipText("Press to perview");
+		convert.setToolTipText("Press to start convert. It could take long time depends on ur PC");
+		
+		//Memonic set
+		stop.setMnemonic(KeyEvent.VK_S);
+		preview.setMnemonic(KeyEvent.VK_P);
+		convert.setMnemonic(KeyEvent.VK_C);
 	}
 }
