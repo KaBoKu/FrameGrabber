afb23fbd9afa550157f08e094cd768c3fd8798d2
Kamil <kk.up.krk@gmail.com>
2013-11-22 13:35:02 +0100

    MainFrame created

--- FrameGrabber/src/uj/edu/pl/gui/MainFrame.java
+++ FrameGrabber/src/uj/edu/pl/gui/MainFrame.java
@@ -1,5 +1,22 @@
 package uj.edu.pl.gui;
 
-public class MainFrame {
 
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	private JButton stop;
+	private JButton preview;
+	private JButton convert;
+	public MainFrame() {
+		super("Frame Grabber");
+		
+		Dimension dimension = new Dimension(810, 320);
+		this.setSize(dimension);
+		this.setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 }
