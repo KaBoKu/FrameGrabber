b35edeb129c79b5b984ac674d17cf5fde3b08646
Kamil <kk.up.krk@gmail.com>
2013-11-22 13:38:12 +0100

    Working...

--- FrameGrabber/src/uj/edu/pl/gui/MainFrame.java
+++ FrameGrabber/src/uj/edu/pl/gui/MainFrame.java
@@ -1,7 +1,8 @@
 package uj.edu.pl.gui;
 
-
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,9 +11,13 @@ public class MainFrame extends JFrame {
 	private JButton stop;
 	private JButton preview;
 	private JButton convert;
+	private GridBagConstraints gBC;
+	private GridBagLayout gBL;
+
 	public MainFrame() {
 		super("Frame Grabber");
-		
+		gBC = new GridBagConstraints();
+		gBL = new GridBagLayout();
 		Dimension dimension = new Dimension(810, 320);
 		this.setSize(dimension);
 		this.setResizable(false);
