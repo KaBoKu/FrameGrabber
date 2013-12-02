5076b4c85fad928362ddfb32d384e04006af7698
Kamil <kk.up.krk@gmail.com>
2013-12-02 13:15:03 +0100

    Thread comunitaction

---
+++ FrameGrabber/src/uj/edu/pl/gui/state/threads/MonitorSOG.java
@@ -0,0 +1,15 @@
+package uj.edu.pl.gui.state.threads;
+
+import uj.edu.pl.gui.StateOfGUI;
+
+public class MonitorSOG {
+	StateOfGUI stateOfGUI = StateOfGUI.INSTANCE;
+	
+	public void read(){
+		
+	}
+	
+	public void write(){
+		
+	}
+}
